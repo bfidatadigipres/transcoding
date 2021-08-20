c14489e52618b68dc1d17a7170c4b65270c62b3e
digitensions <pittschick@me.com>
2021-08-20 10:50:06 +0100

    Script following development/test review

--- source_delay_identifier.sh
+++ source_delay_identifier.sh
@@ -1,39 +1,33 @@
-#!/bin/bash -x
+#!/bin/bash
 
-path_check1="$PATH_INN"
-path_check2="$PATH_MX1"
-path_check3="$PATH_DC1"
-path_check4="$PATH_LMH"
-path_check5="$PATH_VDM"
-log_path="${SCRIPT_LOG}source_delay.log"
+PATH1="$PATH_INN"
+LOG="${SCRIPT_LOG}source_delay_locator1.log"
 
-find "${path_check1}" "${path_check2}" "${path_check3}" "${path_check4}" "${path_check5}" -name "*.mov" | while IFS= read -r files; do
-
+find "${PATH1}/validate/" -name "*.mov" | while IFS= read -r files; do
+    echo "$files"
     source_delay=$(mediainfo -f "$files" | grep 'Source_Delay ')
     video_delay=$(echo $source_delay | awk '{ print $3 }')
     audio_delay=$(echo $source_delay | awk '{ print $6 }')
-
-    if [ -z $audio_delay ];
-      then
-        echo "${files} does not have any audio delay output - okay to process"  >> "$log_path"
-        # Move to transcode path
-      else
-        echo "$files has audio delay $audio_delay"  >> "$log_path"
-    fi
-
-    if [ -z $video_delay ];
-      then
-        echo "${files} does not have any video delay output ${audio_delay} - okay to process"  >> "$log_path"
-        # Move to transcode path
-      else
-        echo "$files has video delay: $video_delay"  >> "$log_path"
-    fi
+    echo " " >> "$LOG"
+    echo "------------------------ $files ------------------------" >> "$LOG"
     if [[ $video_delay == $audio_delay ]];
       then
-        echo "${files}: Audio delay ${audio_delay} and video delay ${video_duration} match - okay to process" >> "$log_path"
-        # Move to transcode path
+        echo "$files: Delay [0] $audio_delay and delay [1] $video_duration match - okay to process" >> "$LOG"
       else
-        echo "WARNING: ${files}  Audio delay and video delay did not match. Moving to Errors folder." >> "$log_path"
-        # Move to error path, as delays don't match which will cause transcode failure
+        echo "WARNING: Delay [0] and delay [1] did not match. Checking if both values are populated" >> "$LOG"
+        if [ -z $video_delay ];
+          then
+            echo "$files does not have any video delay output - okay to process"  >> "$LOG"
+          else
+            echo "$files has delay in mediainfo data [0] assumed to be video: $video_delay"  >> "$LOG"
+            if [ -z $audio_delay ];
+              then
+                echo "$files does not have any audio delay output - okay to process"  >> "$LOG"
+              else
+                echo "$files has delay in mediainfo data [1] assumed to be audio: $audio_delay."  >> "$LOG"
+                echo "WARNING: video/audio delay are populated and do not match. Moving to failures folder: ${PATH1}/failures/source_delay/" >> "$LOG"
+                mv "$files" "${PATH1}/failures/source_delay/"
+            fi
+        fi
     fi
 done
